6744a9a81c13c37aed8d8a3fdb5c133879f9012a
IsmaStormZ <charifou.ismael@gmail.com>
2017-07-17 13:31:26 +0200

    test part 3

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const sendmail = require('sendmail')({      // Send mail without SMTP server
     }});
 
 const app = express();
-const port = Number(process.env.PORT || 3000); // set the environment variable PORT to tell your web server what port to listen on,
-                                               // whatever is in the environment variable PORT, or 3000 if there's nothing there.
+const port = Number(process.env.PORT || 3000); // set the environment variable PORT to tell your web server what port to listen on, whatever is in the environment variable PORT, or 3000 if there's nothing there.
+const userMail = String(process.env.USER_MAIL);
 
 //Parsed the text as JSON
 
@@ -53,8 +53,9 @@ app.post('/contact', (req, res) => {
 
     let mailOptions = {
         from: "cloudunit@treeptik.com",                         // sender address
-        to: "Treeptik mail - <onifuerte@gmail.com>",            // list of receivers
-        subject: 'Cloudunit new test part 2.',
+        // to: "Treeptik mail - <onifuerte@gmail.com>",         // list of receivers
+        to: userMail,                                               // list of receivers
+        subject: 'Cloudunit new test part2.',
         html: "<b>" + "Name : " + req.body.setName + "<b>" + "<br>" + "Mail : " + req.body.setEmail   // name to form in index.html
     };
 
@@ -67,7 +68,6 @@ app.post('/contact', (req, res) => {
 
 // redirect to cloudunit site to try
 
-    // res.redirect("contact-success.html");
     res.redirect("https://cu01.cloudunit.io/#/login");
 });
 
